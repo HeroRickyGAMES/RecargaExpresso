6d168d95e44dedf1c245e9803cdc760e93d2ce8d
HeroRickyGAMES <ricojn9@gmail.com>
2022-11-10 13:06:20 -0300

    Primeira Commit

---
+++ app/src/main/java/com/herorickystudios/recargaexpresso/ActivityWeb.java
@@ -0,0 +1,37 @@
+package com.herorickystudios.recargaexpresso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ActivityWeb extends AppCompatActivity {
+
+    //Encapsulacion de tipo default
+    //No necesitamos un acceso tan restringido como private
+    WebView webV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        webV=(WebView)findViewById(R.id.webView);
+
+        //URL do site
+        String url= "recargaexpresso.com.br/app/";
+
+        //WebViewClient() para que se abra dentro de la aplicacion
+        webV.setWebViewClient(new WebViewClient());
+
+        //Iniciamos la busqueda
+        webV.loadUrl("https://"+url);
+        webV.getSettings().setJavaScriptEnabled(true);
+        webV.getSettings().setAllowContentAccess(true);
+
+        //Esconde a action Bar
+        getSupportActionBar().hide();
+
+    }
+}
